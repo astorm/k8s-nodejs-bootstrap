48d85a857663bba6bc0479bee63c31279dc0cee8
Alan Storm <astorm@alanstorm.com>
2022-06-03 13:21:28 -0700

    fix: post test adjustments

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # k8s-nodejs-bootstrap
-bootstraping a nodejs service in a k8s cluster with kind
+
+A small script that bootstraps a Node.js service in a k8s cluster running via `kind`.
+
+## Getting Started
+
+If you're lucky just run
+
+    % ./bootstrap.sh
+    # lots of output and installing!
+
+If you want to see the post install instructions again
+
+    ./bootstrap.sh postinstall
+
+## More Information
+
+This script will
+
+1. Use `kind` to create a K8s cluster with an exposed port (`32525`)
+2. Install the K8s Dashboard into that cluster
+3. Create an admin user and role binding for the K8s Dashboard
+4. Build a docker container image from the Node.js service in `./service` (exposed on port 8080)
+5. Load that just built image into your `kind` cluster
+6. Create a "K8s Deployment" based on that image
+7. Exposes that K8s Deployment via a "K8s NodePort Service" via port `32525`, forwarded to `8080`
+8. Allows you to invoke your Node.js program from the outside world via `curl http://localhost:32525`
+
+
+## A Note on Dashboard Tokens
+
+Kubernetes 1.24 changed how tokens for the dashboard are generated.  The `postinstall` script above contains _both_ command [for generating a token](https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md).
+
+However -- if your `kubectl` version and your cluster version are out of sync (a common case with the Docker provided `kubectl` or a version of `kind` that's been sitting on your computer for a while) you may not be able to install a token.  We recommend syncing both versions to the latest version available.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 depcheck() {
   echo "checking for needed commands"
   ## check to see if all required commands are present
+
+  # Docker Desktop!  Both the best and worst thing to happen to containers
+  # https://www.docker.com/products/docker-desktop/
+  docker --version > /dev/null
+
+  ## Install: https://kind.sigs.k8s.io/docs/user/quick-start/
   kind version > /dev/null
+
+  ## a version comes preinstalled with docker, but it's an older version
+  ## which may cause isses with pre/post 1.24 feature.
+  ## Install: https://kubernetes.io/docs/tasks/tools/#kubectl
   kubectl version > /dev/null
+
+  # Node and NPM usually come bundled together
+  # node version manager: https://github.com/nvm-sh/nvm
+  # official dists: https://nodejs.org/en/download/
   npm --version > /dev/null
   node --version > /dev/null
-  docker --version > /dev/null
 }
 
 install() {
@@ -47,23 +60,39 @@ install() {
 }
 
 postinstall() {
+  set +x
   echo '+--------------------------------------------------+'
   echo 'To access the dashbaord, run'
   echo '    kubectl proxy'
   echo 'and the load the following URL in your browser'
   echo '    http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/'
   echo '+--------------------------------------------------+'
-  echo 'To get a key for your dashboard, run the following small command'
+  echo 'To get a key for your dashboard, run one of the the following commands'
+  echo '    # pre kubernetes 1.24'
   echo '    kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"'
-  echo 'Command may be different in k8s v1.24+, see'
+  echo '    # post-kubernetes 1.24'
+  echo '    kubectl -n kubernetes-dashboard create token admin-user'
+  echo 're: 1.24 -- this is both your kubernetes server version AND kubectl version'
+  echo 'if one is pre 1.24 and other is post 1.24 weird things will happen'
+  echo '    kubectl version --client # get client version'
+  echo '    kubectl get node -owide  # get cluster/server version'
+  echo ' '
+  echo 'See for more dashbaord user information'
   echo '    https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md'
   echo '+--------------------------------------------------+'
-  echo 'Service will be avaiable outside of the cluster via'
+  echo 'Service will be avaiable "outside of the cluster" via'
   echo '    curl http://localhost:32525'
-  echo 'See the following URL for bash-ing into individual pods'
+  echo 'See the following URL for bash-ing into individual pods for "inside of'
+  echo 'the cluster" access'
   echo '    https://kubernetes.io/docs/tasks/debug/debug-application/get-shell-running-container/'
   echo '+--------------------------------------------------+'
 }
+
+if [ $1 == "postinstall" ]
+then
+  postinstall
+  exit 0
+fi
 
 depcheck
 install
